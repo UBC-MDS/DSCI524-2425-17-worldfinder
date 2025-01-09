399ee6635b07291644833c3825d48f224504bd29
kexins23 <kexins23@student.ubc.ca>
2025-01-09 15:48:09 -0800

    add docstring of getCountries and update README

--- src/worldfinder/worldfinder.py
+++ src/worldfinder/worldfinder.py
@@ -18,3 +18,25 @@ def getCapital(country):
     'Rome'
     """
     pass
+
+def getCountries(city):
+    """
+    Return all unique countries that contain a given city.
+
+    Parameters
+    ----------
+    city : str
+        The name of the city to search for.
+
+    Returns
+    -------
+    list of str
+        A list of unique countries (in their original case) that contain the 
+        specified city. The list will be deduplicated.
+
+    Examples
+    --------
+    >>> getCountries("London")
+    ["Canada", "England", "United States", "Australia", "South Africa", "Jamaica"]
+    """
+    pass
