b3dd21464347774d6956eeae0e0d498e853761be
Michael Gelfand <mgelfa@student.ubc.ca>
2025-01-16 11:56:41 -0800

    Initial function update

--- src/worldfinder/_internals.py
+++ src/worldfinder/_internals.py
@@ -1,10 +1,16 @@
 from importlib.resources import files
 import pandas as pd
 
-def load_countries_data():
-    """
-    Load the countries data from the CSV file
 
+def load_data(csv_name):
+    """
+    Load data from the CSV file
+    
+    Parameters:
+    -----------
+    csv_name : str
+        File name of the dataset to be loaded
+    
     Returns:
     --------
     DataFrame
@@ -12,25 +18,8 @@ def load_countries_data():
 
     Examples:
     ---------
-    load_countries_data()
+    load_data("countries.csv")
 
     """
-    csv_path = files("worldfinder.data").joinpath("countries.csv")
-    return pd.read_csv(csv_path)
-
-def load_cities_data():
-    """
-    Load the cities data from the CSV file.
-
-    Returns:
-    --------
-    DataFrame
-        A Pandas DataFrame containing the cities data
-
-    Examples:
-    ---------
-    load_cities_data()
-
-    """
-    csv_path = files("worldfinder.data").joinpath("cities.csv")
+    csv_path = files("worldfinder.data").joinpath(csv_name)
     return pd.read_csv(csv_path)
