47d965c365781585b412b722db926d7db9906f56
bchangs99 <brianchang071899@gmail.com>
2025-01-15 20:20:22 -0800

    Part of previous commit

--- tests/test_get_capital.py
+++ tests/test_get_capital.py
@@ -1,10 +1,63 @@
 from worldfinder.get_capital import get_capital
+import pytest
 
 def test_capital_correct():
     """
-    Test the correct capital is being returned
+    Test the correct capital is being returned when an input is a string, not empty, and the country exists
     """
     expected = "Rome"
     actual = get_capital('Italy')
 
     assert actual == expected
+
+def test_capital_input_type_error():
+    """
+    Test non string inputs throws an error
+    """
+    with pytest.raises(TypeError, match='Expected a string as input, instead received a'):
+        get_capital(12)
+
+def test_capital_input_empty_string():
+    """
+    Test empty string inputs throws an error
+    """
+
+    with pytest.raises(ValueError, match='Country passed was an empty string'):
+        get_capital("")
+
+def test_case_insensitive_inputs():
+    """
+    Test that the string inputs work in upper/lower/mixed case
+    """
+
+    expected = 'Rome'
+    actual_upper = get_capital('ITALY')
+    actual_lower = get_capital('italy')
+    actual_mixed = get_capital('iTaLy')
+
+    assert expected == actual_upper == actual_lower == actual_mixed
+
+def test_variants_of_country_name():
+    """
+    Test that other forms of the country name should fail
+    """
+
+    with pytest.raises(ValueError, match='Country passed does not exist in our data. Please check your spelling or other variants of the country name'):
+        get_capital('USA')
+
+def test_non_country_passed():
+    """
+    Test that a non-existent country should fail
+    """
+    with pytest.raises(ValueError, match='Country passed does not exist in our data. Please check your spelling or other variants of the country name'):
+        get_capital('Portlandia')
+
+def test_trailing_and_leading_whitespace():
+    """
+    Test that trailing and leading whitespaces 
+    """
+
+    expected = 'Tokyo'
+    actual = get_capital(' Japan   ')
+
+    assert expected == actual
