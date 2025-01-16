adca63ec9b07d060b0570d1a5ac7691f72a3d513
Elaine Chu <chuelaine23@gmail.com>
2025-01-15 23:59:34 -0800

    Added all unit test

--- tests/test_check_city.py
+++ tests/test_check_city.py
@@ -1,27 +1,53 @@
 import pytest
 from worldfinder.check_city import check_city
 
-def test_return_true():
+def test_type_error():
     """
-    Test if it returns True correctly
+    Test if function returns type error if type of input is incorrect
+    """
+    with pytest.raises(TypeError, match="City input must be a string."):
+        check_city(123, "Nepal")
+
+    with pytest.raises(TypeError, match="Country input must be a string."):
+        check_city("Michigan", 45.6)
+
+def test_value_error():
+    """
+    Test if function returns value error if value of input is incorrect
+    """
+    with pytest.raises(ValueError, match="Input city is not in database, please ensure correct spelling or try alternative names."):
+        check_city("Stardew", "Nepal")
+
+    with pytest.raises(ValueError, match="Input country is not in database, please ensure correct spelling or try alternative names."):
+        check_city("Michigan", "Hyrule")
+
+def test_return_results():
+    """
+    Test if it returns results correctly
     """
     assert check_city("London", "Canada") == True
-
-def test_return_false():
-    """
-    Test if it returns False correctly
-    """
     assert check_city("Vancouver", "China") == False
+    assert check_city("London", "United Kingdom") == True
 
-def test_city_case_sensitivity():
+def test_case_sensitivity():
     """
-    Test if it handles case sensitivity in city input correctly
+    Test if it handles case sensitivity in input correctly
     """
     assert check_city("PARis", "France") == True
-
-def test_country_case_sensitivity():
-    """
-    Test if it handles case sensitivity in country input correctly
-    """
     assert check_city("Seattle", "UnitED staTEs") == True
 
+def test_blank_space():
+    """
+    Test if it handles trailing blank spaces in input correctly
+    """
+    assert check_city(" Bangkok  ", "Thailand") == True
+    assert check_city("Manila", "  Philippines  ") == True
+
+def test_empty_string():
+    """
+    Test if function returns value error if input is empty
+    """
+    with pytest.raises(ValueError, match="city cannot be an empty string"):
+        check_city("", "Germany")
+    with pytest.raises(ValueError, match="country cannot be an empty string"):
+        check_city("Seoul", "")    
